525a20b8e3db05b6c0f8533a3057fc8674f7f644
Kuunda Simuchimba <109965577+simuchimba@users.noreply.github.com>
2022-12-20 00:15:50 +0200

    Add files via upload

---
+++ welcome.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+#include<windows.h>
+#include<unistd.h>
+using namespace std;
+void welcome();
+void intro();
+
+
+
+  void welcome()
+  {
+
+  cout<<"\n\n\n\n\n\n\n ";
+
+  cout<<"\t\tKUUNDA'S MONEY BANKING SYSTEM"<<endl;
+  sleep(2);
+
+  intro();
+  sleep(2);
+
+  }
+
+ void intro()
+ {
+
+  cout<<"\t\tWelcome to KuuTech Bank"<<endl;
+ }
+
